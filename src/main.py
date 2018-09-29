de15966255c8f3faf8603523fa8527c9f01aef3a
Derek Zhang <derekzh4ng@outlook.com>
2018-09-28 22:09:09 -0400

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -17,10 +17,15 @@ class Game:
 		self.clock = pygame.time.Clock()
 
 		self.current_scene = Scene(exbg)
-		self.current_scene.add_dialogue(Dialogue("How are you doing",[Response("well",2),Response("bad",3),Response("fuck u",4)]))
-		self.player_choice = None
+		self.current_scene.add_dialogue(Dialogue("D0",[Response("z",1),Response("z",1),Response("z",1)],exchar))
+		self.current_scene.add_dialogue(Dialogue("RANDOM GIRL: Hi! I haven't seen you around ... nice to meet you!",[Response("Likewise! What's your name?",2),Response("Ew, get away from me",3),Response("Gtfo cuz u know u a thot",4)],exchar))
+		self.current_scene.add_dialogue(Dialogue("D2",[Response("3",3),Response("4",4),Response("1",1)],exchar))
+		self.current_scene.add_dialogue(Dialogue("D3",[Response("4",4),Response("1",1),Response("2",2)],exchar))
+		self.current_scene.add_dialogue(Dialogue("D4",[Response("1",1),Response("2",2),Response("3",3)],exchar))
 
 		self.game_state = "splash"
+
+		self.text_anim = True
 
 	def run(self):
 		# Game Loop
@@ -35,6 +40,7 @@ class Game:
 							self.game_state = "speaking"
 
 				self.window.fill(BLACK)
+				self.text("S P L A S H S C R E E N", "Courier New",30,WHITE,0,0,align="center")
 				pygame.display.update()
 
 			while self.game_state == "speaking":
@@ -46,8 +52,34 @@ class Game:
 						if event.key == pygame.K_SPACE:
 							self.game_state = "replying"
 
-				pygame.draw.rect(self.window, GREY, (200,510,WIDTH-400,150),5)
-				self.current_scene.current_dialogue.play_text(self.window)
+				if self.game_state != "speaking":
+					break
+
+				self.window.blit(self.current_scene.bg,(0,0))
+
+				# Draw Character
+				self.window.blit(self.current_scene.current_dialogue.image,(0,0))
+
+				overlay = pygame.Surface((680,150));overlay.set_alpha(128);overlay.fill(BLACK)
+				self.window.blit(overlay,(200,510))
+
+				if self.text_anim:
+					self.current_scene.current_dialogue.play_text(self.window,self.current_scene.bg,overlay,210,520)
+					self.text_anim = False
+				else:
+					tt = self.current_scene.current_dialogue.t
+					text_font = pygame.font.SysFont("arial", 25)
+					message = text_font.render(tt, True, WHITE)
+
+					if message.get_width() > WIDTH-410:
+						#split the message into sections 
+						index = int(len(tt)*(WIDTH-410)/(message.get_width()))
+						t1 = tt[:find_space_backwards(tt,index)]
+						t2 = tt[find_space_backwards(tt,index):]
+						self.text(t1,"arial",25,WHITE,210,520)
+						self.text(t2.strip(),"arial",25,WHITE,210,550)
+					else:
+						self.text(tt,"arial",25,WHITE,210,520)
 
 				pygame.display.update()
 				self.clock.tick(FPS)
@@ -58,23 +90,39 @@ class Game:
 					if event.type == pygame.QUIT:
 						self.close()
 					if event.type == pygame.MOUSEBUTTONUP:
+						# Check for button clicks
 						if mx >= 200 and mx <= WIDTH-200:
-							if my > 510 and my < 560:
-								print(1)
-							elif my > 560 and my < 610:
-								print(2)
-							elif my > 610 and my < 660:
-								print(3)
+							if my > 510 and my < 660:
+								if my > 510 and my < 560:
+									print(1)
+									self.current_scene.input(1)
+								elif my > 560 and my < 610:
+									self.current_scene.input(2)
+								elif my > 610 and my < 660:
+									self.current_scene.input(3)
+								self.text_anim = True
+								self.game_state = "speaking"
 
+				if self.game_state != "replying":
+					break
+
+				# Clear screen
 				self.window.blit(self.current_scene.bg,(0,0))
 
+				# Draw Character
+				self.window.blit(self.current_scene.current_dialogue.image,(0,0))
+
+				# Grab mouse position
 				mx,my = pygame.mouse.get_pos()
 
+				# Declaring the faded overlay and selector
 				overlay = pygame.Surface((680,150));overlay.set_alpha(128);overlay.fill(BLACK)
 				selector = pygame.Surface((680,50));selector.set_alpha(128);selector.fill(BLACK)
 
+				# Overlay Render
 				self.window.blit(overlay,(200,510))
 
+				# Selector Render
 				if mx >= 200 and mx <= WIDTH-200:
 					if my > 510 and my < 560:
 						self.window.blit(selector,(200,510))
@@ -83,22 +131,27 @@ class Game:
 					elif my > 610 and my < 660:
 						self.window.blit(selector,(200,610))
 
-				'''pygame.draw.rect(self.window, BACK, (200,510,WIDTH-400,50),3)
-				pygame.draw.rect(self.window, WHITE, (200,560,WIDTH-400,50),3)
-				pygame.draw.rect(self.window, WHITE, (200,610,WIDTH-400,50),3)'''
+				# Actual Replies
+				for i in range(3): 
+					t = self.current_scene.current_dialogue.responses[i].text
+					
+					self.text(t, "arial", 25, WHITE, 210, 518+50*i)
+
 
 				pygame.display.update()
 				self.clock.tick(FPS)
 
-	def text(self,text, font, size, color, x, y):
+	def text(self,text, font, size, color, x, y, align="free"):
 	    font_style = str(font)
 	    font_size = size
 
 	    text_font = pygame.font.SysFont(font_style, font_size)
 
 	    message = text_font.render(text, True, color)
-
-	    self.window.blit(message, (x, y))
+	    if(align == "center"):
+	    	self.window.blit(message, (WIDTH/2-message.get_width()//2, HEIGHT/2-message.get_height()//2))
+	    else:
+	    	self.window.blit(message, (x, y))
 
 	def close(self):
 		pygame.quit()
--- src/settings.py
+++ src/settings.py
@@ -7,9 +7,15 @@ WIDTH, HEIGHT = 1080,720
 TITLE = "Dating Sim"
 FPS = 60
 
+TEXT_SCROLL_SPEED = 0.005 # in seconds
+
 # Image Loading
 path = "../resources/images/"
 exbg = pygame.image.load("{}example_bg.jpg".format(path))
+exchar = pygame.image.load("{}example_char.png".format(path))
+
+# Font Definitions
+comicsans50 = pygame.font.SysFont("comicsansms", 50)
 
 # Colour Definitions
 BLACK = (0,0,0)
@@ -20,3 +26,9 @@ BLUE = (0,0,255)
 YELLOW = (255,255,0)
 AQUA = (0,255,255)
 GREY = (200,200,200)
+
+# Fancy Functions
+def find_space_backwards(string, index):
+	for i in range(index,0,-1):
+		if string[i] == ' ':
+			return i
