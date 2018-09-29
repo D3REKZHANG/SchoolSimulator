7ea6d48d9688ce4ef9dd150390c6ec8034c7712e
Derek Zhang <derekzh4ng@outlook.com>
2018-09-29 09:44:33 -0400

    Add files via upload

--- src/classes.py
+++ src/classes.py
@@ -1,4 +1,5 @@
-import pygame
+import pygame,time
+from settings import *
 
 class Player:
 	def __init__(self,name):
@@ -6,12 +7,88 @@ class Player:
 		self.score
 
 class Dialogue:
-	def __init__(self, text, responses):
-		self.text = text
+	def __init__(self, text, responses, image):
+		self.t = text
 		self.responses = responses
+		self.image = image
 
-	def play_text(self,window):
-		pygame.draw.rect(window, (200,120,30), (100,100,200,200))
+	def play_text(self,window,bg,overlay,x,y):
+		text_font = pygame.font.SysFont("arial", 25)
+		message = text_font.render(self.t, True, WHITE)
+
+		if message.get_width() > WIDTH-410:
+			#split the message into sections
+			index = int(len(self.t)*(WIDTH-410)/(message.get_width()))
+			t1 = self.t[:find_space_backwards(self.t,index)]
+
+			if(message.get_width()/(WIDTH-410))>2:
+				index2 = int((len(self.t)-index)*(WIDTH-410)/(message.get_width()))
+				t2 = self.t[find_space_backwards(self.t,index):find_space_backwards(self.t,index2)].strip()
+				t3 = self.t[find_space_backwards(self.t,index2):].strip()
+			else:
+				t2 = self.t[find_space_backwards(self.t,index):].strip()
+
+			for i in range(len(t1)):
+				if self.check_break():
+					break
+				window.blit(bg,(0,0))
+				window.blit(overlay,(200,510))
+				window.blit(self.image,(0,0))
+				self.text(window, t1[:i],"arial", 25, WHITE, x, y)
+				time.sleep(TEXT_SCROLL_SPEED)
+				pygame.display.update()
+			for i in range(len(t2)):
+				if self.check_break():
+					break
+				window.blit(bg,(0,0))
+				window.blit(overlay,(200,510))
+				window.blit(self.image,(0,0))
+				self.text(window, t1,"arial",25,WHITE,x,y)
+				self.text(window, t2[:i],"arial", 25, WHITE, x, y+30)
+				time.sleep(TEXT_SCROLL_SPEED)
+				pygame.display.update()
+			if(message.get_width()/(WIDTH-400)>2):
+				for i in range(len(t3)):
+					if self.check_break():
+						break
+					window.blit(bg,(0,0))
+					window.blit(overlay,(200,510))
+					window.blit(self.image,(0,0))
+					self.text(window, t1,"arial",25,WHITE,x,y)
+					self.text(window, t2,"arial", 25, WHITE, x, y+30)
+					self.text(window, t3[:i],"arial", 25, WHITE, x, y+60)
+
+					time.sleep(TEXT_SCROLL_SPEED)
+					pygame.display.update()
+		else:
+			for i in range(len(self.t)):
+				if self.check_break():
+					break
+				window.blit(bg,(0,0))
+				window.blit(overlay,(200,510))
+				window.blit(self.image,(0,0))
+				self.text(window, self.t[:i],"arial", 25, WHITE, x, y)
+				time.sleep(TEXT_SCROLL_SPEED)
+				pygame.display.update()
+
+	def text(self, window, text, font, size, color, x, y):
+		font_style = str(font)
+		font_size = size
+
+		text_font = pygame.font.SysFont(font_style, font_size)
+		message = text_font.render(text, True, color)
+		
+		window.blit(message, (x, y))
+
+	def check_break(self):
+		for event in pygame.event.get():
+			# check for closing window
+			if event.type == pygame.QUIT:
+				self.close()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_SPACE:
+					return True
+		return False
 
 class Response:
 	def __init__(self, text, target):
@@ -33,4 +110,4 @@ class Scene:
 
 	def input(self, option):
 		''' option -> response 1, 2, or 3'''
-		self.current_dialogue = self.dialogues[self.current_dialogue.responses[option-1]]
+		self.current_dialogue = self.dialogues[self.current_dialogue.responses[option-1].target_id]
